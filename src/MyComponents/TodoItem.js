9453dc4ea325cf567b8cdd0ff2986afd048a475e
anuragK-24 <24sept.anurag@gmail.com>
2023-08-24 16:43:29 +0530

    Changed the button name

--- src/MyComponents/TodoItem.js
+++ src/MyComponents/TodoItem.js
@@ -10,7 +10,7 @@ export const TodoItem = ({todo, onDelete}) => {
           <h4 style={{textAlign:"center"}}>{todo.title}</h4>
           <p>{todo.desc}</p>
           <button className="btn btn-sm btn-danger" onClick={() => {onDelete(todo)}}
-            >Delete</button>
+            >Done</button>
           {/* if we passs onDelete() all three button fire with starting of the website  */}
       </div>
       <hr />

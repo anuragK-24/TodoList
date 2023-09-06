066c7cd5fc331bd83cf63632217d8c8ae5ef27a8
anuragK-24 <24sept.anurag@gmail.com>
2023-09-06 11:32:42 +0530

    Modified TodoItem.js

--- src/MyComponents/TodoItem.js
+++ src/MyComponents/TodoItem.js
@@ -4,7 +4,6 @@ export const TodoItem = ({todo, onDelete}) => {
   return (
     <center>
       <div  style={{border:"1px solid black", borderRadius:"1em",alignItems:"center", backgroundColor:"#00edff",width:"50%" , padding:"1em",textAlign:"center"}}>
-          <h5 style={{textAlign:"initial", block:"none"}}>{todo.sno}</h5>
           <h4 style={{textAlign:"center"}}>{todo.title.toUpperCase()}</h4>
           <p>{todo.desc}</p>
           <button className="btn btn-sm btn-danger" onClick={() => {onDelete(todo)}}
